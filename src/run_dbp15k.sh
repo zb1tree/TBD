96d4f98d39020cec369b0c2f2a2905e1afd3597d
zb1tree <m202271786@hust.edu.cn>
2023-11-23 13:46:39 +0000

    剪切冗余链接

--- src/run_dbp15k.sh
+++ src/run_dbp15k.sh
@@ -19,4 +19,36 @@ echo $paras
 python -u SDEAPreprocess.py $paras
 paras="$paras --fold 0"
 paras="$paras --gpus ${gpus}"
-python -u SDEATrain.py $paras
+python -u TBDTrain.py $paras
+
+# ja_en
+paras=""
+paras="$paras --datasets_root ../data"
+paras="$paras --dataset ja_en"
+paras="$paras --result_root ../outputs"
+paras="$paras --pretrain_bert_path ../pre_trained_models/bert-base-multilingual-uncased"
+paras="$paras --log"
+paras="$paras --relation"
+paras="$paras --version ${version}"
+echo $paras
+
+python -u SDEAPreprocess.py $paras
+paras="$paras --fold 0"
+paras="$paras --gpus ${gpus}"
+python -u TBDTrain.py $paras
+
+# fr_en
+paras=""
+paras="$paras --datasets_root ../data"
+paras="$paras --dataset fr_en"
+paras="$paras --result_root ../outputs"
+paras="$paras --pretrain_bert_path ../pre_trained_models/bert-base-multilingual-uncased"
+paras="$paras --log"
+paras="$paras --relation"
+paras="$paras --version ${version}"
+echo $paras
+
+python -u SDEAPreprocess.py $paras
+paras="$paras --fold 0"
+paras="$paras --gpus ${gpus}"
+python -u TBDTrain.py $paras
